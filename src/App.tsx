62a4b3cf8904a1ec163e88f77d1470e6c298ac46
Andreyhuey <andreyhuey777@gmail.com>
2023-10-09 21:56:08 +0100

    medi Lifi project initiated

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import { useState } from "react";
+
+function App() {
+  return (
+    <>
+      <div className="min-h-screen bg-black">
+        <p className="font-700 text-yellow-400 ">Fresh</p>
+      </div>
+    </>
+  );
+}
+
+export default App;
