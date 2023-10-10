96234c1026c2377541d7f15ef91aca853f7db76c
Andreyhuey <andreyhuey777@gmail.com>
2023-10-10 17:56:26 +0100

    Components architecture modified for efficient importing and exporting

---
+++ src/components/index.ts
@@ -0,0 +1,29 @@
+import About from "./About";
+import Blog from "./Blog";
+import Contact from "./Contact";
+import FAQs from "./FAQs";
+import Features from "./Features";
+import Footer from "./Footer";
+import Gallery from "./Gallery";
+import Header from "./Header";
+import Navbar from "./Navbar";
+import Newsletter from "./Newsletter";
+import Services from "./Services";
+import Team from "./Team";
+import Testimonials from "./Testimonials";
+
+export {
+  About,
+  Blog,
+  Contact,
+  FAQs,
+  Features,
+  Footer,
+  Gallery,
+  Header,
+  Navbar,
+  Newsletter,
+  Services,
+  Team,
+  Testimonials,
+};
