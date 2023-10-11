215260280c8c851ac33d441927d1ec0f35519cd3
Andreyhuey <andreyhuey777@gmail.com>
2023-10-11 21:47:22 +0100

    Navbar component completed for tabs and larger devices

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,9 +1,75 @@
-import React from "react";
+import { useState } from "react";
+import { Link, Button } from "react-scroll";
 
 const Navbar = () => {
+  const [nav, setNav] = useState(false);
+  const handleNav = () => setNav(!nav);
+
   return (
-    <div>
-      <></>
+    <div className="bg-[#CCE0FF] lg:px-12 md:px-8 px-4 py-3">
+      {/* for pcs and tabs */}
+      <div className="hidden md:flex items-center justify-between">
+        <ul className="flex items-center md:gap-12 lg:gap-16">
+          <Link className="h1 hover:cursor-pointer">MED</Link>
+          <Link
+            activeClass="active"
+            to=""
+            spy={true}
+            smooth={true}
+            offset={50}
+            duration={500}
+            className="hover:cursor-pointer"
+          >
+            <li className="">About</li>
+          </Link>
+          <Link
+            activeClass="active"
+            to=""
+            spy={true}
+            smooth={true}
+            offset={50}
+            duration={500}
+            className="hover:cursor-pointer"
+          >
+            <li className="">Features</li>
+          </Link>
+          <Link
+            activeClass="active"
+            to=""
+            spy={true}
+            smooth={true}
+            offset={50}
+            duration={500}
+            className="hover:cursor-pointer"
+          >
+            Services
+          </Link>
+          <Link
+            activeClass="active"
+            to=""
+            spy={true}
+            smooth={true}
+            offset={50}
+            duration={500}
+            className="hover:cursor-pointer"
+          >
+            Blog
+          </Link>
+        </ul>
+        <Button
+          className="text-[#478CF7] border-[1px] border-[#478df75e] px-6 py-2 hover:scale-110 trans"
+          activeClass="active"
+          to="contact"
+          spy={true}
+          smooth={true}
+          offset={50}
+          duration={500}
+        >
+          Contact
+        </Button>
+      </div>
+
+      {/* for phones */}
     </div>
   );
 };
