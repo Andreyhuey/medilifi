0e2479ad41829d3f6e23027749acab051390cd1b
Andreyhuey <andreyhuey777@gmail.com>
2023-10-23 18:53:30 +0100

    team data imported and arranged

---
+++ src/data.ts
@@ -0,0 +1,27 @@
+import image1 from "./assets/image1.png";
+import image2 from "./assets/image2.png";
+import image3 from "./assets/image3.png";
+import image4 from "./assets/image4.png";
+
+export const team = [
+  {
+    img: image1,
+    name: "Anton Brownstein",
+    role: "Doctor",
+  },
+  {
+    img: image2,
+    name: "Farid Amini",
+    role: "Nurse",
+  },
+  {
+    img: image3,
+    name: "Rita Leite",
+    role: "Therapist",
+  },
+  {
+    img: image4,
+    name: "Sergio Pliego",
+    role: "Technician",
+  },
+];
