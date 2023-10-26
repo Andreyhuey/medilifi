0f425a08a36ee6d1139aa37fae77096a5f42c6bb
Andreyhuey <andreyhuey777@gmail.com>
2023-10-26 21:21:07 +0100

    Contact component added to App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -6,6 +6,7 @@ import {
   Services,
   Team,
   Footer,
+  Contact,
 } from "./components";
 
 function App() {
@@ -16,6 +17,7 @@ function App() {
       <About />
       <Features />
       <Services />
+      <Contact />
       <Team />
       <Footer />
     </>
--- src/components/Contact.tsx
+++ src/components/Contact.tsx
@@ -1,9 +1,16 @@
-import React from "react";
-
 const Contact = () => {
   return (
-    <div>
-      <></>
+    <div className="min-h-screen section ">
+      <div></div>
+      <div>
+        <p className="text-[#A1A7B0]">CONTACT</p>
+        <h1 className="lg:text-[56px] text-[46px] font-bold lg:leading-[64px] leading-[50px] lg:max-w-[396px] text-center">
+          Get In Touch With Us
+        </h1>
+      </div>
+      <div className="flex flex-col justify-start">
+        <div></div>
+      </div>
     </div>
   );
 };
