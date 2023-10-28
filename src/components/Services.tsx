2cf24030f7e619b4a02e05d1331cad793d461a8a
Andreyhuey <andreyhuey777@gmail.com>
2023-10-28 17:56:06 +0100

    Services component modified

--- src/components/Services.tsx
+++ src/components/Services.tsx
@@ -5,52 +5,52 @@ import icon7 from "../assets/icon7.png";
 
 const Services = () => {
   return (
-    <div className="min-h-screen section bg-white">
+    <div className="min-h-screen section bg-white w-full">
       <div className="flex flex-col justify-center items-center gap-5">
         <p className="text-[#A1A7B0]">SERVICES </p>
-        <h1 className="lg:text-[56px] text-[46px] font-bold lg:leading-[64px] leading-[50px] lg:max-w-[396px] text-center">
+        <h1 className="lg:text-[56px] text-[46px] font-bold lg:leading-[64px] leading-[50px] lg:w-full text-center">
           Field of Health and Healing
         </h1>
       </div>
-      <div className="grid grid-cols-1 lg:grid-cols-2 py-10 lg:px-20 gap-5 justify-center">
-        <div className="flex items-center justify-center h-[256px] lg:w-[475px] shadow-md">
-          <div className="bg-[#EEFAFF] flex items-center justify-center h-full w-full">
-            <img src={icon4} alt="icon4" />
+      <div className="grid grid-cols-1 md:grid-cols-2 py-10 lg:px-20 gap-4 md:gap-8 mt-4 lg:mt-8">
+        <div className="flex items-center justify-center  lg:w-[100%] shadow-md">
+          <div className="bg-[#EEFAFF] flex items-center justify-center w-full h-full">
+            <img src={icon4} alt="icon4" className="object-contain w-fit" />
           </div>
-          <div className="p-6 flex flex-col items-start h-full w-full justify-between">
+          <div className="p-6 flex flex-col items-start h-full w-full justify-between gap-12 lg:gap-20">
             <p className="font-bold ">Health Promotion</p>
             <p className="text-[#33333369]">
               Organized medical service offering
             </p>
           </div>
         </div>
-        <div className="flex items-center justify-center h-[256px] lg:w-[475px] shadow-md">
-          <div className="bg-[#EEFAFF] flex items-center justify-center h-full w-full">
-            <img src={icon5} alt="icon4" />
+        <div className="flex items-center justify-center  lg:w-[100%] shadow-md">
+          <div className="bg-[#EEFAFF] flex items-center justify-center w-full h-full">
+            <img src={icon5} alt="icon4" className="object-contain w-fit" />
           </div>
-          <div className="p-6 flex flex-col items-start h-full w-full justify-between">
+          <div className="p-6 flex flex-col items-start h-full w-full justify-between gap-12 lg:gap-20">
             <p className="font-bold ">Health Promotion</p>
             <p className="text-[#33333369]">
               Organized medical service offering
             </p>
           </div>
         </div>
-        <div className="flex items-center justify-center h-[256px] lg:w-[475px] shadow-md">
-          <div className="bg-[#EEFAFF] flex items-center justify-center h-full w-full">
-            <img src={icon6} alt="icon4" />
+        <div className="flex items-center justify-center  lg:w-[100%] shadow-md">
+          <div className="bg-[#EEFAFF] flex items-center justify-center w-full h-full">
+            <img src={icon6} alt="icon4" className="object-contain w-fit" />
           </div>
-          <div className="p-6 flex flex-col items-start h-full w-full justify-between">
+          <div className="p-6 flex flex-col items-start h-full w-full justify-between gap-12 lg:gap-20">
             <p className="font-bold ">Health Promotion</p>
             <p className="text-[#33333369]">
               Organized medical service offering
             </p>
           </div>
         </div>
-        <div className="flex items-center justify-center h-[256px] lg:w-[475px] shadow-md">
-          <div className="bg-[#EEFAFF] flex items-center justify-center h-full w-full">
-            <img src={icon7} alt="icon4" />
+        <div className="flex items-center justify-center  lg:w-[100%] shadow-md">
+          <div className="bg-[#EEFAFF] flex items-center justify-center w-full h-full">
+            <img src={icon7} alt="icon4" className="object-contain w-fit" />
           </div>
-          <div className="p-6 flex flex-col items-start h-full w-full justify-between">
+          <div className="p-6 flex flex-col items-start h-full w-full justify-between gap-12 lg:gap-20">
             <p className="font-bold ">Health Promotion</p>
             <p className="text-[#33333369]">
               Organized medical service offering
