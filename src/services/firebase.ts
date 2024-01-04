7ae0b33cbc9550a0363dfe1d9f4cae7e941c4584
Andreyhuey <andreyhuey777@gmail.com>
2024-01-04 21:56:34 +0100

    firebase details configured

---
+++ src/services/firebase.ts
@@ -0,0 +1,19 @@
+import { initializeApp } from "firebase/app";
+import { getAuth } from "firebase/auth";
+import { getFirestore } from "firebase/firestore";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyA04NpEkWHSWAFn62poqwXSpFt-UAP7B0E",
+  authDomain: "medilifi-a63d4.firebaseapp.com",
+  databaseURL: "https://medilifi-a63d4-default-rtdb.firebaseio.com",
+  projectId: "medilifi-a63d4",
+  storageBucket: "medilifi-a63d4.appspot.com",
+  messagingSenderId: "592571138835",
+  appId: "1:592571138835:web:c31cb671a003483bc310e6",
+};
+
+const app = initializeApp(firebaseConfig);
+
+export const auth = getAuth(app);
+
+export const db = getFirestore(app);
