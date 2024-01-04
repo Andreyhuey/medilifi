dc25d57e3a3cd69883c9f510bd98eb93d0677e8f
Andreyhuey <andreyhuey777@gmail.com>
2024-01-04 22:12:45 +0100

    firebase authentication procedure modified

--- src/app/store.ts
+++ src/app/store.ts
@@ -1,13 +1,14 @@
-import { configureStore } from "@reduxjs/toolkit";
+// import { configureStore } from "@reduxjs/toolkit";
+// import authSlice from "../services/authSlice";
 
-import { patientDataApi } from "../services/patientDataApi";
+// import { patientDataApi } from "../services/patientDataApi";
 
-export default configureStore({
-  reducer: {
-    [patientDataApi.reducerPath]: patient.reducer,
-  },
-  middleware: (getDefaultMiddleware) =>
-    getDefaultMiddleware({
-      serializableCheck: false,
-    }).concat(patientDataApi.middleware),
-});
+// export default configureStore({
+//   // reducer: {
+//   //   [patientDataApi.reducerPath]: patient.reducer,
+//   // },
+//   middleware: (getDefaultMiddleware) =>
+//     getDefaultMiddleware({
+//       serializableCheck: false,
+//     }).concat(patientDataApi.middleware),
+// });
