92ee11922445353811b9768316b5381adb883bce
Andreyhuey <andreyhuey777@gmail.com>
2024-01-16 00:25:37 +0100

    update: vitals reset to john doe

--- src/pages/Dashboard.tsx
+++ src/pages/Dashboard.tsx
@@ -27,8 +27,8 @@ const Dashboard = () => {
   // original working code
   useEffect(() => {
     if (!user) return navigate("/login");
-    // const vitalsRef = ref(db, `users/John Doe/vitals`);
-    const vitalsRef = ref(db, `users/${userProp?.name}/vitals`);
+    const vitalsRef = ref(db, `users/John Doe/vitals`);
+    // const vitalsRef = ref(db, `users/${userProp?.name}/vitals`);
     const listener = onValue(
       vitalsRef,
       (snapshot) => {
@@ -43,7 +43,6 @@ const Dashboard = () => {
     );
 
     setIsLoading(false);
-    console.log(userProp?.name);
     return () => listener();
   }, [user, userProp, db]);
 
@@ -59,6 +58,16 @@ const Dashboard = () => {
         {error && <p className="text-red-500 text-lg">{error}</p>}
         <div className="  bg-[#CCE0FF] rounded-lg w-full h-full">
           <div className="flex flex-col lg:flex-row items-center justify-center  gap-5 p-10 lg:p-20 h-full w-full lg:min-w-fit">
+            <div className="flex flex-col items-center justify-center bg-[#F4FCFF] gap-3 p-6 rounded-lg w-full h-full">
+              <FaTemperatureHigh className="text-7xl text-[#478CF7]" />
+              <div className=" text-center">
+                <p className=" font-bold pb-2">Temperature: </p>
+                <p className="font-medium text-[#478CF7]">
+                  {vitals?.tempC} <span className="text-black">°C</span>
+                </p>
+              </div>
+            </div>
+
             <div className="flex flex-col items-center justify-center bg-[#F4FCFF] gap-3  p-6 rounded-lg w-full h-full">
               <FaHeartPulse className="text-7xl text-[#478CF7]" />
               <div className=" text-center">
@@ -75,16 +84,6 @@ const Dashboard = () => {
                 <p className=" font-bold pb-2">Oxygen Saturation Level: </p>
                 <p className="font-medium text-[#478CF7]">
                   {vitals?.spO2} <span className="text-black">SpO₂</span>
-                </p>
-              </div>
-            </div>
-
-            <div className="flex flex-col items-center justify-center bg-[#F4FCFF] gap-3 p-6 rounded-lg w-full h-full">
-              <FaTemperatureHigh className="text-7xl text-[#478CF7]" />
-              <div className=" text-center">
-                <p className=" font-bold pb-2">Temperature: </p>
-                <p className="font-medium text-[#478CF7]">
-                  {vitals?.tempC} <span className="text-black">°C</span>
                 </p>
               </div>
             </div>
--- src/pages/index.ts
+++ src/pages/index.ts
@@ -1,6 +1,5 @@
 import Homepage from "./Homepage";
-import SignUp from "./SignUp";
 import Login from "./Login";
 import Dashboard from "./Dashboard";
 
-export { Homepage, SignUp, Login, Dashboard };
+export { Homepage, Login, Dashboard };
